936c7725f295b660b402eab3271c79688511d483
Selith <selithrubasingha@gmail.com>
2026-04-06 19:25:42 +0530

    wrote the main chip 8 interface

---
+++ chip8.cpp
@@ -0,0 +1,19 @@
+#include <cstdint>
+
+class Chip8
+{
+    public :
+        uint8_t registers[16];
+        uint8_t memory[4096];
+        uint16_t index{};
+        uint16_t pc{};
+        uint16_t stack[16];
+        uint8_t sp{};
+        uint8_t SoundTimer{};
+        uint8_t delayTimer{};
+        uint32_t video[64*32]{};
+        uint8_t keypad[16]{};
+        uint16_t opcode;
+
+
+}
