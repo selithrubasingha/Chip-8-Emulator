c4bd40fd29d4f2fa908703d4c0aa9cea60c161f1
Selith <selithrubasingha@gmail.com>
2026-04-07 19:23:18 +0530

    load ROM method

--- chip8.cpp
+++ chip8.cpp
@@ -7,4 +7,23 @@ void Chip8::LoadROM(char const* filename){
     //open the file and move the cursor to the end of file
     // ate = at the end
     std::ifstream file(filename , std::ios::binary | std::ios::ate);
+
+    if (file.is_open()){
+        //get the size of the file and allocate a buffer to it 
+        std::streampos size = file.tellg();
+        char* buffer = new char[size];
+
+        //go back to the beginning to the file and start reading baby !
+        file.seekg(0,std::ios::beg);
+        file.read(buffer , size);
+        file.close();
+
+        //  YOu need to load these data into the chip 8 's memory
+        for (long i = 0; i< size ; ++i){
+            memory[START_ADDRESS + i] = buffer[i];
+        }
+
+        //you need to free the pointers always 
+        delete[] buffer;
+    }
 }
--- chip8.hpp
+++ chip8.hpp
@@ -15,16 +15,9 @@ class Chip8
         uint8_t keypad[16]{};
         uint16_t opcode;
 
+        void LoadROM(char const* filename);
+
 
 };
 
 
-class Chip8 {
-public:
-    // ... your registers and memory go here ...
-
-    // ADD THIS LINE:
-    void LoadROM(char const* filename); 
-
-    // ... timers, video, etc. ...
-}; // <--- The class ends here
