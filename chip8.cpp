32354d21033dee21547bf9a545e804ea820654d2
Selith <selithrubasingha@gmail.com>
2026-04-09 15:09:14 +0530

    feat :  set Vx = Vx OR Vy

--- chip8.cpp
+++ chip8.cpp
@@ -161,6 +161,14 @@ void Chip8::OP_8xy0(){
     registers[Vx] = registers[Vy];
 }
 
+/// @brief set Vx = Vx OR Vy
+void Chip8::OP_8xy1(){
+    uint16_t Vx = (opcode && 0x0F00u);
+    uint16_t Vy = (opcode && 0x00F0u);
+
+    registers[Vx]  = registers[Vx] | registers[Vy];
+}
+
 
 
 
