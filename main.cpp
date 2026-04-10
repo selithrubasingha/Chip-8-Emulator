32626f2d6015e8e25cf6c05b19a06a07ab69a790
Selith <selithrubasingha@gmail.com>
2026-04-10 19:04:43 +0530

    feat :  platform class header file added

---
+++ main.cpp
@@ -0,0 +1,4 @@
+#include "chip8.hpp"
+#include <iostream>
+#include "platform.cpp"
+
---
+++ platform.hpp
@@ -0,0 +1,22 @@
+#ifndef PLATFORM_HPP
+#define PLATFORM_HPP
+
+#include <SDL2/SDL.h>
+#include <cstdint>
+
+class Platform
+{
+public:
+    Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+    ~Platform();
+
+    void Update(void const* buffer, int pitch);
+    bool ProcessInput(uint8_t* keys);
+
+private:
+    SDL_Window* window{};
+    SDL_Renderer* renderer{};
+    SDL_Texture* texture{};
+};
+
+#endif
