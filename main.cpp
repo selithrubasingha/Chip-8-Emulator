3374d7c7567904d41fcc5c686d08f938f5385dab
Selith <selithrubasingha@gmail.com>
2026-04-10 19:15:07 +0530

    docs : commented the main function

--- main.cpp
+++ main.cpp
@@ -11,10 +11,12 @@ int main(int argc, char* argv[])
     
     }
 
+    //these arguments should be given in the program 
     int videoScale = std::stoi(argv[1]);
     int cycleDelay = std::stoi(argv[2]);
     char const* romFilename = argv[3];
 
+    //platform is the connector of teh cpp code and the display (GPU)
     Platform platform("CHIP-8 Emulator",VIDEO_WIDTH * videoScale ,VIDEO_HEIGHT* videoScale , VIDEO_WIDTH,VIDEO_HEIGHT);
 
     Chip8 chip8;
@@ -27,17 +29,23 @@ int main(int argc, char* argv[])
     bool quit = false;
 
     while (!quit){
+        //quit will become true if escape key or X(in window) is pressed
         quit = platform.ProcessInput(chip8.keypad);
 
+
+        //if timedelay is 30ms , when dt hits 30ms , the cycle will execute . then the freq is 1/30ms !!
         auto currentTime = std::chrono::high_resolution_clock::now();
         float dt = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastCycleTime).count();
 
         if (dt > cycleDelay){
             chip8.Cycle();
             lastCycleTime = currentTime;
+
+            //update the screen
+            platform.Update(chip8.video, videoPitch);
         }
 
-        platform.Update(chip8.video, videoPitch);
+        
     }   
 
     return 0;
