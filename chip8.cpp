32247fa525a4df1fbebe313b2d15950eb88c5aec
Selith <selithrubasingha@gmail.com>
2026-04-10 17:46:51 +0530

    feat :  initlized the instruction tables in the chip8 constructor

--- chip8.cpp
+++ chip8.cpp
@@ -67,6 +67,71 @@ void Chip8::LoadROM(char const* filename){
 
         //you need to free the pointers always 
         delete[] buffer;
+
+        /*
+        storing the 34 instruction is done using several tables ...
+        it is like a tree structure where in the opcode code if the first hexNum is nto 
+        0,8,E or F then we fo to the next subTables ... otherwise we can directly get the instructions . 
+
+        */
+
+        table[0x0] = &Chip8::Table0;
+        table[0x1] = &Chip8::OP_1nnn;
+        table[0x2] = &Chip8::OP_2nnn;
+        table[0x3] = &Chip8::OP_3xkk;
+        table[0x4] = &Chip8::OP_4xkk;
+        table[0x5] = &Chip8::OP_5xy0;
+        table[0x6] = &Chip8::OP_6xkk;
+        table[0x7] = &Chip8::OP_7xkk;
+        table[0x8] = &Chip8::Table8;
+        table[0x9] = &Chip8::OP_9xy0;
+        table[0xA] = &Chip8::OP_Annn;   
+        table[0xB] = &Chip8::OP_Bnnn;
+        table[0xC] = &Chip8::OP_Cxkk;
+        table[0xD] = &Chip8::OP_Dxyn;
+        table[0xE] = &Chip8::TableE;
+        table[0xF] = &Chip8::TableF;    
+
+        //initializing the sub-tables with the corresponding functions
+        for (size_t i = 0; i < 0xE + 1 ; ++i){
+            table0[i] = &Chip8::OP_NULL;
+            table8[i] = &Chip8::OP_NULL;
+            tableE[i] = &Chip8::OP_NULL;
+        }
+
+        table0[0x0] = &Chip8::OP_00E0;
+        table0[0xE] = &Chip8::OP_00EE;
+
+        table8[0x0] = &Chip8::OP_8xy0;
+        table8[0x1] = &Chip8::OP_8xy1;
+        table8[0x2] = &Chip8::OP_8xy2;
+        table8[0x3] = &Chip8::OP_8xy3;
+        table8[0x4] = &Chip8::OP_8xy4;
+        table8[0x5] = &Chip8::OP_8xy5;
+        table8[0x6] = &Chip8::OP_8xy6;
+        table8[0x7] = &Chip8::OP_8xy7;
+        table8[0xE] = &Chip8::OP_8xyE;
+
+        tableE[0x1] = &Chip8::OP_Ex9E;
+        tableE[0xA] = &Chip8::OP_ExA1;
+
+        //the F table is the biggest one so we initialize it with a loop and then we fill the rest of the values with the corresponding functions
+        // the F table has two variables the last two hex nums ... it could theoretically go upto FF which is 255
+        // but in the instruction set the max is 65 which is 102 in decimal . 
+        for (size_t i = 0; i < 0x65 + 1 ; ++i){
+            tableF[i] = &Chip8::OP_NULL;
+        }
+
+        tableF[0x07] = &Chip8::OP_Fx07;
+        tableF[0x0A] = &Chip8::OP_Fx0A;
+        tableF[0x15] = &Chip8::OP_Fx15;
+        tableF[0x18] = &Chip8::OP_Fx18;
+        tableF[0x1E] = &Chip8::OP_Fx1E;
+        tableF[0x29] = &Chip8::OP_Fx29;
+        tableF[0x33] = &Chip8::OP_Fx33;
+        tableF[0x55] = &Chip8::OP_Fx55;
+        tableF[0x65] = &Chip8::OP_Fx65;
+
     }
 }
 
